95a354e15b956c5827e993e36194718f291bf058
Illia Kokhanovskiy <ilya.kohanovskiy2004@gmail.com>
2023-04-16 12:59:52 +0300

    Base controller

---
+++ server/src/common/route.interface.ts
@@ -0,0 +1,11 @@
+import { NextFunction, Router, Response, Request } from 'express';
+import { IMiddleware } from './middleware.interface';
+
+export interface IControllerRoute {
+	path: string;
+	func: (req: Request, res: Response, next: NextFunction) => void;
+	method: keyof Pick<Router, 'get' | 'post' | 'delete' | 'patch' | 'put'>;
+	middlewares: IMiddleware[];
+}
+
+export type ExpressResponse = Response<any, Record<string, any>>;
