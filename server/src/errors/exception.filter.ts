1c2179aea91896fe52d00fc3c09c1932a5cdd85e
Illia Kokhanovskiy <ilya.kohanovskiy2004@gmail.com>
2023-04-16 13:00:26 +0300

    Error handling

---
+++ server/src/errors/exception.filter.ts
@@ -0,0 +1,20 @@
+import { ILoggerService } from '../logger/logger.service.interface';
+import { DITypes } from '../DITypes';
+import { inject } from 'inversify';
+import { IExceptionFilter } from './exception.filter.interface';
+import { HttpError } from './http-error.class';
+import { NextFunction, Request, Response } from 'express';
+
+export class ExceptionFilter implements IExceptionFilter {
+	constructor(@inject(DITypes.LoggerService) private LoggerService: ILoggerService) {}
+
+	catch(err: Error | HttpError, req: Request, res: Response, next: NextFunction): void {
+		if (err instanceof HttpError) {
+			this.LoggerService.error(`[${err.context}] Error ${err.statusCode}: ${err.message}`);
+			res.status(err.statusCode).send({ err: err.message });
+		} else {
+			this.LoggerService.error(` Error: ${err.message}`);
+			res.status(500).send({ err: err.message });
+		}
+	}
+}
